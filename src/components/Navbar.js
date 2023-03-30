540f24b93ee1831f15b397242e8ac74282df5d87
marinayasinsky <122933750+marinayasinsky@users.noreply.github.com>
2023-03-30 15:37:13 -0400

    day3

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { Link, animateScroll as scroll } from 'react-scroll';
+import { animateScroll as scroll } from 'react-scroll';
 
 const Navbar = () => {
   const scrollToTop = () => {
