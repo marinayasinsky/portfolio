2f910d77f24e7d820e532c64d6787bd17df08b57
marinayasinsky <122933750+marinayasinsky@users.noreply.github.com>
2023-03-31 17:38:16 -0400

    friday

--- src/components/About.js
+++ src/components/About.js
@@ -1,27 +1,30 @@
 import React from 'react';
 import { Container, Row, Col, Button } from 'react-bootstrap';
 import '../styles/Home.css';
+import resume from '../assets/Resume.docx';
 
 const About = () => {
   return (
-    <div className="about">
-      <div className="page-header">
-        <h1>About Me</h1>
+  <>
+  <div className="col-12 text-center my-4">
+        <h1 className="page-header lg=4 mb-0">About Me</h1>
       </div>
+    <div className="about">
       <Container>
         <Row>
           <Col>
-            <p>Currently, in training as a Software Engineering Bootcamp student(graduating May 2023), I have built a strong foundation in HTML, CSS, JavaScript, Node.js, Express.js, MongoDB, Mongoose, React, SQL, and computer science concepts.</p>
-            <p>With over 10 years of experience in business development and account management. Accomplished and results-driven professional with expertise in customer-focused cultures, market research, and sales revenue strategies. Articulate communicator with a track record of boosting loyalty and profits.</p>
-            <p>Excited to apply my unique blend of technical and business skills to drive success for your team.</p>
+          <p>As a software engineering bootcamp student with over 10 years of experience in business development and account management, I have developed a strong foundation in HTML, CSS, JavaScript, Node.js, Express.js, MongoDB, Mongoose, React, SQL, and computer science concepts.</p>
+<p>I am a results-driven professional with expertise in customer-focused cultures, market research, and sales revenue strategies. My articulate communication skills have helped me build and foster productive relationships with clients, resulting in increased loyalty and profits.</p>
+<p>I am passionate about the tech industry and my work. I enjoy working on challenging projects and strive to continuously improve my skills and knowledge. As a detail-oriented and adaptable individual with a strong work ethic, I am confident in my ability to achieve my professional goals and make valuable contributions to any team.</p>
+<p>In addition to my software engineering training, I hold a combined Bachelor's/Master's degree in Economics and have completed a certificate program in International Business. I am also proficient in MS Office, QuickBooks, and Salesforce.</p>
           </Col>
         </Row>
         <Row>
           <Col>
             <div className="resume">
-              <h2>Resume</h2>
+              <h4>Resume</h4>
               <p>Download my resume:</p>
-              <Button variant="primary" href="/path/to/resume.pdf" download>
+              <Button variant="secondary" href={resume}  download>
                 Download Resume
               </Button>
             </div>
@@ -29,6 +32,7 @@ const About = () => {
         </Row>
       </Container>
     </div>
+    </>
   );
 };
 
